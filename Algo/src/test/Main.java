6b8d020bf5b442dfafacf0769580a00c0861195e
강민기 <131340181+KangMinGi92@users.noreply.github.com>
2025-05-02 18:28:55 +0900

    algo 수정

--- Algo/src/test/Main.java
+++ Algo/src/test/Main.java
@@ -1,15 +1,24 @@
 package test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			if (a > 0 && b > 0 && a < 10 && b < 10) {
-				System.out.println(a + b);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int s = Integer.parseInt(br.readLine());
+		for(int i=0;i<s;i++) {
+			int a = Integer.parseInt(br.readLine());
+			int b = Integer.parseInt(br.readLine());
+			int c = a+b;
+			String str=Integer.valueOf(c).toString();
+			bw.write(str);
 		}
+		bw.close();
 	}// main
 }// class
 
