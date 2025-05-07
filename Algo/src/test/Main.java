5c62ea4ee7eac768b458038b72343bc2a43e90af
강민기 <131340181+KangMinGi92@users.noreply.github.com>
2025-05-08 00:44:06 +0900

    algo 수정

--- Algo/src/test/Main.java
+++ Algo/src/test/Main.java
@@ -1,34 +1,26 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int x = sc.nextInt();
-		List<Integer> list = new ArrayList<>();
-		List<Integer> result = new ArrayList<>();
-		Random r = new Random();
-		for(int i=0;i<n;i++) {
-			list.add(r.nextInt(10000)+1);
-		}
-		for(Integer i : list) {
-			if(i<x) {
-				result.add(i);
-			}
-		}
-		
-		for(Integer s : result) {
-			System.out.println(s);
-		}
+		char c = sc.next().charAt(0);
+		System.out.println((int)c);
 	}// main
-}// class		
+}// class	
 
+/*
+알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+입력
+알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+
+출력
+입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+
+*/
 			//throws IOException 던져야 사용가능하다.
 //        // 빠른 입력
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
