d4b20928cbcef146e4ad07a0d5b62881dd261b49
강민기 <131340181+KangMinGi92@users.noreply.github.com>
2025-05-09 18:30:37 +0900

    알고리즘 수정

--- Algo/src/test/Main.java
+++ Algo/src/test/Main.java
@@ -1,27 +1,41 @@
 package test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		char c = sc.next().charAt(0);
-		System.out.println((int)c);
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		String str = br.readLine();
+		char[] chars = str.toCharArray();
+		for (Character c : chars) {
+			if (Character.isUpperCase(c)) {
+				sb.append(Character.toLowerCase(c));
+			} else {
+				sb.append(Character.toUpperCase(c));
+			}
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}// main
-}// class	
+}// class
 
 /*
-알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
-
-입력
-알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
-
-출력
-입력으로 주어진 글자의 아스키 코드 값을 출력한다.
-
-*/
-			//throws IOException 던져야 사용가능하다.
+ * 영어 소문자와 대문자로 이루어진 단어를 입력받은 뒤, 대문자는 소문자로, 소문자는 대문자로 바꾸어 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력 첫째 줄에 영어 소문자와 대문자로만 이루어진 단어가 주어진다. 단어의 길이는 최대 100이다.
+ * 
+ * 출력 첫째 줄에 입력으로 주어진 단어에서 대문자는 소문자로, 소문자는 대문자로 바꾼 단어를 출력한다.
+ * 
+ */
+// throws IOException 던져야 사용가능하다.
 //        // 빠른 입력
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        // 빠른 출력
@@ -40,5 +54,3 @@ public class Main {
 //        bw.flush();
 //        bw.close();
 //        br.close();
-
-
